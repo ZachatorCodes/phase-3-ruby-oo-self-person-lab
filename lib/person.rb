0b7a7aa6b1b754e2b99768f9a67b780d6b9f3bf5
Zach Schwartz <zachatorcodes@gmail.com>
2023-04-11 17:03:53 -0600

    formatted lab

--- lib/person.rb
+++ lib/person.rb
@@ -37,40 +37,39 @@ class Person
     self.hygiene > 7
   end
 
-  def get_paid amount
+  def get_paid(amount)
     self.bank_account += amount
     "all about the benjamins"
   end
 
   def take_bath
     self.hygiene += 4
-    '♪ Rub-a-dub just relaxing in the tub ♫'
+    "♪ Rub-a-dub just relaxing in the tub ♫"
   end
 
   def work_out
     self.hygiene -= 3
     self.happiness += 2
-    '♪ another one bites the dust ♫'
+    "♪ another one bites the dust ♫"
   end
 
-  def call_friend friend
+  def call_friend(friend)
     self.happiness += 3
     friend.happiness += 3
     "Hi #{friend.name}! It's #{self.name}. How are you?"
   end
 
-  def start_conversation friend, topic
-    if topic == 'politics'
+  def start_conversation(friend, topic)
+    if topic == "politics"
       self.happiness -= 2
       friend.happiness -= 2
-      'blah blah partisan blah lobbyist'
-    elsif topic == 'weather'
+      "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
       self.happiness += 1
       friend.happiness += 1
-      'blah blah sun blah rain'
+      "blah blah sun blah rain"
     else
-      'blah blah blah blah blah'
+      "blah blah blah blah blah"
     end
   end
-
 end
